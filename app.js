2e357d789d3864f4a5b2fbe41f06eb77357011e7
rchillida <rchillida@gmail.com>
2023-07-15 23:47:03 -0300

    notas

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 const express = require('express');
 const app = express();
+
+/* Puerto */
 const PORT = 3001;
 
-app.get('/home', (req, res) => res.send('Server corriendo desde express al home'));
-app.get('/', (req, res) => res.send('pong'));
+/* Import de las rutas */
+const mainRoutes = require('./src/routes/mainRoutes')
+const shopRoutes = require('./src/routes/shopRoutes');
 
+/* Define carpeta de archivos estáticos */
+app.use(express.static('public'));
+
+/* Parsea los datos recibidos por POST */
+app.use(express.urlencoded());
+app.use(express.json());
+
+/* Middleware a las Rutas */
+app.use('/', mainRoutes);
+app.use('/', shopRoutes);
+
+/* Middleware para manejar el error 404 */
+app.use((req, res, next) => {
+    res.status(404).sendFile(__dirname+'/public/pages/404.html');
+});
+
+/* Método para correr el server */
 app.listen(PORT, () => console.log(`Escucho el puerto ${PORT}`));
--- node.js
+++ node.js
@@ -1,16 +1,16 @@
-// crear un servidor
+// // crear un servidor
 
-const http = require('http');
-const fs = require('fs');
+// const http = require('http');
+// const fs = require('fs');
 
-const server = http.createServer((req, res) => {
-    const file = fs.readFileSync("./public/index.html")
+// const server = http.createServer((req, res) => {
+//     const file = fs.readFileSync("./public/index.html")
 
-    res.writeHead(200, {
-        "Content-Type": "text/html; charset=UTF-8"
-    });
+//     res.writeHead(200, {
+//         "Content-Type": "text/html; charset=UTF-8"
+//     });
 
-    res.end(file);
-});
+//     res.end(file);
+// });
 
-server.listen(3000, () => console.log("Servidor corriendo en puerto 3000"));
+// server.listen(3000, () => console.log("Servidor corriendo en puerto 3000"));
