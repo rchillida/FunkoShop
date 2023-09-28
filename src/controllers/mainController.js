2f1a813ed1f33f23cc7303cb3bb526cd879c29e9
rchillida <rchillida@gmail.com>
2023-09-27 22:23:42 -0300

    cleaning

--- src/controllers/mainController.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    home: (req, res) => res.send('Pagina Home'),
-    shop: (req, res) => res.send('Pagina Shop'),
-    Item: (req, res) => res.send('Pagina Item'),
-    
-};
--- src/services/itemServices.js
+++
@@ -1,9 +0,0 @@
-const items = require('../models/items');
-
-const getItems = async (params) => {
-    return items.getItems(params);
-};
-
-module.exports = {
-    getItems
-}
